d89ddf00c5f1578f4730986ac63426b023e0b7f6
Orlando Challenger <orlandofat@gmail.com>
2020-05-08 19:17:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,9 +2,9 @@ def using_push(array, string)
   array.push (string)
 end
 
-def using_unshift(array, string)
-  array.unshift (string)
-end
+#def using_unshift(array, string)
+#  array.unshift (string)
+#end
 
 def
 end
